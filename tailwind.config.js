3b5765b0a03e92ed8bbd7197fe96f4a45c8c2943
garv1908 <garv1908@gmail.com>
2025-02-22 21:27:14 -0500

    frontend works after merge, colour issue solved through tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+/* @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}', './src/**/**/*.{js,ts,jsx,tsx}, ./src/**/**/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0466C8',
+        'deep-blue': '#0353A4',
+        'dark-blue': '#023E7D',
+        'navy-blue': '#002855',
+        'dark-1': '#001845',
+        'dark-2': '#001233',
+        'gray-1': '#33415C',
+        'gray-2': '#5C677D',
+        'accent-1': '#7D8597',
+        'accent-2': '#979DAC',
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
